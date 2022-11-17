c3b28ff7d7982df4e6fc961bd7a020fd3fa8c715
Jozef Chutka <jozefchutka@gmail.com>
2022-11-17 15:25:41 +0100

    added 0.6.4-rc5

---
+++ ffmpeg/0.6.4-rc5/run.bat
@@ -0,0 +1,38 @@
+@ECHO off
+
+REM 7z https://github.com/mcmilk/7-Zip-zstd
+REM brotli https://mirror.msys2.org/mingw/mingw32/mingw-w64-i686-brotli-1.0.9-5-any.pkg.tar.zst
+
+IF NOT EXIST wasm.tar (
+	ECHO wasm.tar not found
+	PAUSE
+	EXIT
+)
+
+IF NOT EXIST ffmpeg.exe (
+	ECHO ffmpeg.exe not found
+	PAUSE
+	EXIT
+)
+
+IF NOT EXIST .htaccess (
+	ECHO .htaccess not found
+	PAUSE
+	EXIT
+)
+
+7z x wasm.tar -aoa
+DEL wasm.tar
+
+ECHO. >> .htaccess
+ECHO # Generated extra >> .htaccess
+
+for /r %%i in (*.exe, *.js, *.wasm) do (
+	brotli %%i -o %%i.br -Z -f
+
+	ECHO ^<FilesMatch "(%%~nxi\.br)$"^> >> .htaccess
+	ECHO 	Header set x-content-length %%~zi >> .htaccess
+	ECHO ^</FilesMatch^> >> .htaccess
+
+	DEL %%i
+)
